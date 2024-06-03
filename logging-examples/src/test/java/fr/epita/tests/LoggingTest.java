6264da2f7628b4da07683a85af17f0bb16863cba
Présentation <tbr@tbr.com>
2024-06-03 10:01:38 +0200

    log4j2 integration

--- logging-examples/src/test/java/fr/epita/tests/LoggingTest.java
+++ logging-examples/src/test/java/fr/epita/tests/LoggingTest.java
@@ -4,6 +4,8 @@ package fr.epita.tests;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Date;
+
 public class LoggingTest {
 
     private static final Logger LOGGER = LogManager.getLogger(LoggingTest.class);
@@ -11,6 +13,19 @@ public class LoggingTest {
 
     public static void main(String[] args) {
         LOGGER.debug("test message");
+        LOGGER.debug("test message for : {}, at {}", "this is a parameter message or an eval", new Date());
+        LOGGER.debug(()-> new Date());
+
+        try{
+            Integer.parseInt("0.0");
+        }catch (Exception e){
+            LOGGER.error("this is a provoked error", e);
+            //avoid this:
+//            e.printStackTrace();
+//            System.out.println(e);
+        }
+
+
     }
 
 }
