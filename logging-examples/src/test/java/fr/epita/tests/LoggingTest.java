c8fb1499bc283d0ef28924a3b4abf7472eca0abd
Présentation <tbr@tbr.com>
2024-06-03 09:41:40 +0200

    ldap connection

---
+++ logging-examples/src/test/java/fr/epita/tests/LoggingTest.java
@@ -0,0 +1,16 @@
+package fr.epita.tests;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(LoggingTest.class);
+
+
+    public static void main(String[] args) {
+        LOGGER.info("test");
+    }
+
+}
