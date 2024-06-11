56bca8c95410ce113b81630c3334430e2931537f
Présentation <tbr@tbr.com>
2024-06-11 10:44:18 +0200

    fix hibernate configuration

--- dependency-injection/src/main/java/fr/epita/di/services/impl/JPADoctorDAO.java
+++ dependency-injection/src/main/java/fr/epita/di/services/impl/JPADoctorDAO.java
@@ -3,10 +3,12 @@ package fr.epita.di.services.impl;
 import fr.epita.di.datamodel.Doctor;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 public class JPADoctorDAO {
 
 
+    @PersistenceContext
     EntityManager em;
 
     public void create(Doctor d){
