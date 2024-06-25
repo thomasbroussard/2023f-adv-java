0deb6cd07acc9a32462d8be7507793392f3f3dd2
Présentation <tbr@tbr.com>
2024-06-25 11:53:35 +0200

    spring boot integration and http method

--- dependency-injection/src/main/java/fr/epita/di/app/PatientController.java
+++ dependency-injection/src/main/java/fr/epita/di/app/PatientController.java
@@ -4,6 +4,8 @@ import fr.epita.di.app.conversion.Mappers;
 import fr.epita.di.app.messages.PatientDTO;
 import fr.epita.di.datamodel.Patient;
 import fr.epita.di.services.impl.DataService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @RestController
 public class PatientController {
+
+    private static final Logger LOGGER = LogManager.getLogger(PatientController.class);
 
     @Autowired
     DataService service;
@@ -28,7 +32,7 @@ public class PatientController {
 
     @GetMapping(value = "/api/patients/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PatientDTO> createPatient(@PathVariable("id") Integer id){
-        System.out.println(id);
+        LOGGER.info("retrieving patient with id {}", id);
         //TODO log
         Patient patient = service.getPatient(id);
         return ResponseEntity.ok( Mappers.dtoFromPatient(patient));
